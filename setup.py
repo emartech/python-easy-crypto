df5c05b0d0ece5c9313ae64f795c1d7dfffa814d
Jozsa Csongor <csongor.jozsa@emarsys.com>
2018-12-07 13:26:09 +0100

    chore(build): set up deploying on tag
    Co-authored-by: Ildiko Czeller <czeildi@gmail.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='easycrypto',
     description='Provides simple wrappers around Python\'s easycrypto implementation.',
-    version='0.0.1',
+    version='0.0.2',
     author='Emarsys Security',
     author_email='security@emarsys.com',
     license='MIT',
